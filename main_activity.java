cdd5913f8818e8ebc69717ffb2e42f2e7bd15a6d
samuel souza <124898734+sammsouzaa@users.noreply.github.com>
2023-04-13 14:25:11 -0300

    Update main_activity.java

--- main_activity.java
+++ main_activity.java
@@ -1,5 +1,6 @@
 package com.example.jokenpooo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
@@ -10,17 +11,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-
     public EditText idJogador1;
     public EditText idJogador2;
-
     public TextView idVencedor;
-
     public TextView idNomeJogador1;
     public TextView idNomeJogador2;
-
     public ImageView idImg1;
     public ImageView idImg2;
 
@@ -53,30 +49,44 @@ public class MainActivity extends AppCompatActivity {
         // 2 tesoura
         // 3 pedra
 
-        //Casos que o jogador 1 ganha:
-
         idNomeJogador1.setText(jogador1);
         idNomeJogador2.setText(jogador2);
 
-        if (IDnum1 == IDnum2){
-            idVencedor.setText("EMPATE");
+        //Casos de empate:
 
+        if (IDnum1 == 1 && IDnum2 == 1 ){
+            idVencedor.setText("EMPATE");
+            idImg1.setImageResource(R.drawable.papel);
+            idImg2.setImageResource(R.drawable.papel);
         }
+        else if (IDnum1 == 2 && IDnum2 == 2 ){
+            idVencedor.setText("EMPATE");
+            idImg1.setImageResource(R.drawable.tesoura);
+            idImg2.setImageResource(R.drawable.tesoura);
+        }
+        else if (IDnum1 == 3 && IDnum2 == 3 ){
+            idVencedor.setText("EMPATE");
+            idImg1.setImageResource(R.drawable.pedra);
+            idImg2.setImageResource(R.drawable.pedra);
+        }
+
+        //Casos que o jogador 1 ganha:
+
         else if (IDnum1 == 1 && IDnum2 == 3 ) {
 
-            idVencedor.setText("O vencedor é :" + jogador1);
+            idVencedor.setText("O vencedor é: " + jogador1);
             idImg1.setImageResource(R.drawable.papel);
             idImg2.setImageResource(R.drawable.pedra);
         }
         else if (IDnum1 == 2 && IDnum2 == 1 ) {
 
-            idVencedor.setText("O vencedor é :" + jogador1);
+            idVencedor.setText("O vencedor é: " + jogador1);
             idImg1.setImageResource(R.drawable.tesoura);
             idImg2.setImageResource(R.drawable.papel);
         }
         else if (IDnum1 == 3 && IDnum2 == 2 ) {
 
-            idVencedor.setText("O vencedor é :" + jogador1);
+            idVencedor.setText("O vencedor é: " + jogador1);
             idImg1.setImageResource(R.drawable.pedra);
             idImg2.setImageResource(R.drawable.tesoura);
         }
@@ -85,19 +95,19 @@ public class MainActivity extends AppCompatActivity {
 
         else if (IDnum1 == 3 && IDnum2 == 1 ) {
 
-            idVencedor.setText("O vencedor é :" + jogador2);
+            idVencedor.setText("O vencedor é: " + jogador2);
             idImg1.setImageResource(R.drawable.pedra);
             idImg2.setImageResource(R.drawable.papel);
         }
         else if (IDnum1 == 1 && IDnum2 == 2 ) {
 
-            idVencedor.setText("O vencedor é :" + jogador2);
+            idVencedor.setText("O vencedor é: " + jogador2);
             idImg1.setImageResource(R.drawable.papel);
             idImg2.setImageResource(R.drawable.tesoura);
         }
         else if (IDnum1 == 2 && IDnum2 == 3 ) {
 
-            idVencedor.setText("O vencedor é :" + jogador2);
+            idVencedor.setText("O vencedor é: " + jogador2);
             idImg1.setImageResource(R.drawable.tesoura);
             idImg2.setImageResource(R.drawable.pedra);
         }
